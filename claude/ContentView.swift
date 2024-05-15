6965198d5d2e2644be20497702dcdc56b937046c
Daniel Callisaya <danielj.callisayah@gmail.com>
2024-05-15 16:25:54 -0400

    Initial Commit

---
+++ claude/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  claude
+//
+//  Created by Daniel Jesus Callisaya Hidalgo on 15/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ claude/claudeApp.swift
@@ -0,0 +1,17 @@
+//
+//  claudeApp.swift
+//  claude
+//
+//  Created by Daniel Jesus Callisaya Hidalgo on 15/5/24.
+//
+
+import SwiftUI
+
+@main
+struct claudeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
